c837560468ea8fb65481f1fe38788f024dc82a13
Jorn Bostelaar <bostelaarj@gmail.com>
2016-05-18 22:46:04 +0200

    core: Added redux store to app

--- src/client/app.js
+++ src/client/app.js
@@ -1,8 +1,16 @@
 import getRoutes from 'client/utils/getRoutes';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import * as reducers from 'client/reducers';
+
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
 
 render((
-	<Router history={browserHistory} children={getRoutes()} />
+	<Provider store={store}>
+		<Router history={browserHistory} children={getRoutes()} />
+	</Provider>
 ), document.getElementById('app'));
